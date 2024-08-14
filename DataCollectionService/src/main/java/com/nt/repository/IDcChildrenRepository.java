ef18046e7509bfa36184663e89289739c6171a69
amreshpd <amreshpd98@gmail.com>
2024-08-15 00:07:22 +0530

    push by amresh

---
+++ DataCollectionService/src/main/java/com/nt/repository/IDcChildrenRepository.java
@@ -0,0 +1,9 @@
+package com.nt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nt.entity.DcChildrenEntity;
+
+public interface IDcChildrenRepository extends JpaRepository<DcChildrenEntity, Integer> {
+
+}
